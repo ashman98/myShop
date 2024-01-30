646387c4eaf78302504a2f853870d752342974de
Ash <manukyan15199815@gmail.com>
2024-01-30 12:41:25 -0800

    react new

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,13 @@
 import './App.css';
-import { BrowserRouter } from 'react-router-dom';
 import AppRouter from "./components/AppRouter";
 import NavBar from "./components/NavBar";
 
 function App() {
   return (
-    <BrowserRouter className="App">
+      <>
         <NavBar />
         <AppRouter />
-    </BrowserRouter>
+      </>
   );
 }
 
--- client/src/index.js
+++ client/src/index.js
@@ -1,21 +1,31 @@
-import React, { createContext } from 'react';
+import React, {createContext, Suspense} from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import UserStore from "./store/UserStore";
 import DeviceStore from "./store/DeviceStore";
+import {HelmetProvider} from "react-helmet-async";
+import { BrowserRouter } from 'react-router-dom';
+
 
 export const Context = createContext(null);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
+
     <Context.Provider value={{
         user : new UserStore(),
         devices: new DeviceStore(),
     }}>
-     <App />
+        <HelmetProvider>
+            <BrowserRouter>
+                <Suspense fallback={<div>Loading...</div>}> {/*loading ogtagorcelu hnaravorrutyun*/}
+                    <App />
+                </Suspense>
+            </BrowserRouter>
+      </HelmetProvider>
     </Context.Provider>
   </React.StrictMode>
 );
